fc087106f65db6e8702a999c170302c6e91e97b8
timurshkutov <timlooksthere@gmail.com>
2026-06-18 13:56:11 +0500

    Create my_second.py

---
+++ dags/my_second.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2026, 6, 18),
+}
+
+with DAG(
+    dag_id='test_dag_lol',
+    default_args=default_args,
+    schedule='@weekly',
+    catchup=False
+) as dag:
+
+    task1 = BashOperator(
+        task_id='print_hello',
+        bash_command='echo "YO HELLO"'
+    )
